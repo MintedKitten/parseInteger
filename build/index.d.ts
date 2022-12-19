1577e305b8fc728e6dc2d98d06c353cef72bb41c
MintedKitten <87274176+MintedKitten@users.noreply.github.com>
2022-12-19 15:23:10 +0700

    Update declaration and Bundled files

--- build/index.d.ts
+++ build/index.d.ts
@@ -2,6 +2,7 @@
  * Parse input exclusively as Safe Integer
  * @param s {String} The input
  * @throws {RangeError} Input is more than Number.MAX_SAFE_INTEGER
+ * @throws {RangeError} Input is less than Number.MIN_SAFE_INTEGER
  * @throws {TypeError} Input is not an Integer
  * @returns {Number} A Safe Integer
  */
--- build/index.js
+++ build/index.js
@@ -1,22 +1,34 @@
 "use strict";
-exports.__esModule = true;
-exports.parseInteger = void 0;
-/**
- * Parse input exclusively as Safe Integer
- * @param s {String} The input
- * @throws {RangeError} Input is more than Number.MAX_SAFE_INTEGER
- * @throws {TypeError} Input is not an Integer
- * @returns {Number} A Safe Integer
- */
-function parseInteger(s) {
-    var nm = Number(s);
-    var nm2 = parseInt(s);
-    if (nm > Number.MAX_SAFE_INTEGER) {
-        throw new RangeError("Input is more than Number.MAX_SAFE_INTEGER");
-    }
-    if (nm !== nm2 || isNaN(nm)) {
-        throw new TypeError("Input is not an Integer");
-    }
-    return nm;
+var E = Object.defineProperty;
+var N = Object.getOwnPropertyDescriptor;
+var o = Object.getOwnPropertyNames;
+var s = Object.prototype.hasOwnProperty;
+var u = (e, r) => {
+    for (var n in r) E(e, n, { get: r[n], enumerable: !0 });
+  },
+  a = (e, r, n, I) => {
+    if ((r && typeof r == "object") || typeof r == "function")
+      for (let t of o(r))
+        !s.call(e, t) &&
+          t !== n &&
+          E(e, t, {
+            get: () => r[t],
+            enumerable: !(I = N(r, t)) || I.enumerable,
+          });
+    return e;
+  };
+var i = (e) => a(E({}, "__esModule", { value: !0 }), e);
+var p = {};
+u(p, { parseInteger: () => m });
+module.exports = i(p);
+function m(e) {
+  let r = Number(e),
+    n = parseInt(e);
+  if (r > Number.MAX_SAFE_INTEGER)
+    throw new RangeError("Input is more than Number.MAX_SAFE_INTEGER");
+  if (r < Number.MIN_SAFE_INTEGER)
+    throw new RangeError("Input is less than Number.MIN_SAFE_INTEGER");
+  if (r !== n || isNaN(r)) throw new TypeError("Input is not an Integer");
+  return r;
 }
-exports.parseInteger = parseInteger;
+0 && (module.exports = { parseInteger });
