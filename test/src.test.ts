95d18cb199ef72e7701cd7e9d50c51104244440c
MintedKitten <87274176+MintedKitten@users.noreply.github.com>
2023-01-09 18:29:28 +0700

    Change test to use ts-jest

---
+++ test/src.test.ts
@@ -0,0 +1,22 @@
+import { parseInteger } from "../src";
+
+describe("Test Parse Integer", () => {
+  test("Parse Integer Input Scenarios", () => {
+    function test(s: string) {
+      return parseInteger(s);
+    }
+    expect(() => test("")).toThrowError(
+      new TypeError("Input is not an Integer")
+    );
+    expect(() => test("0.5")).toThrowError(
+      new TypeError("Input is not an Integer")
+    );
+    expect(() => test("30000000000000000")).toThrowError(
+      new RangeError("Input is more than Number.MAX_SAFE_INTEGER")
+    );
+    expect(() => test("-30000000000000000")).toThrowError(
+      new RangeError("Input is less than Number.MIN_SAFE_INTEGER")
+    );
+    expect(test("12")).toBe(12);
+  });
+});
