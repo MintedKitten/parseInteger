151f5ad1797d5d42b476959cd7c8ef39e8944200
MintedKitten <87274176+MintedKitten@users.noreply.github.com>
2023-01-14 04:17:06 +0700

    Bumped version to 1.1.1 :\

--- build/index.d.ts
+++ build/index.d.ts
@@ -1,11 +1,10 @@
 /**
  * Parse input exclusively as Safe Integer
  * @param s {String} The input
- * @param radix {Number} The radix. (Optional) Default = 10;
  * @throws {RangeError} Input is more than Number.MAX_SAFE_INTEGER
  * @throws {RangeError} Input is less than Number.MIN_SAFE_INTEGER
  * @throws {TypeError} Input is not an Integer
  * @returns {Number} A Safe Integer
  */
-declare function parseInteger(s: string, radix?: number): number;
+declare function parseInteger(s: string): number;
 export { parseInteger as parseInteger };
