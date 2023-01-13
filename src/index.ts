3077633617be60c598a261ab0075a1921bcb93a7
MintedKitten <87274176+MintedKitten@users.noreply.github.com>
2023-01-14 04:16:17 +0700

    Remove previous changes

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,14 @@
 /**
  * Parse input exclusively as Safe Integer
  * @param s {String} The input
- * @param radix {Number} The radix. (Optional) Default = 10;
  * @throws {RangeError} Input is more than Number.MAX_SAFE_INTEGER
  * @throws {RangeError} Input is less than Number.MIN_SAFE_INTEGER
  * @throws {TypeError} Input is not an Integer
  * @returns {Number} A Safe Integer
  */
-function parseInteger(s: string, radix?: number): number {
-  const rad = radix ?? 10;
+function parseInteger(s: string): number {
   const nm = Number(s);
-  const nm2 = parseInt(s, rad);
+  const nm2 = parseInt(s);
   if (nm > Number.MAX_SAFE_INTEGER) {
     throw new RangeError("Input is more than Number.MAX_SAFE_INTEGER");
   }
