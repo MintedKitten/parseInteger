a64e1ba5cb9c234ef3704e30dc556d8bdceecaae
MintedKitten <87274176+MintedKitten@users.noreply.github.com>
2023-01-14 04:03:14 +0700

    parseInteger: Add optional parameter: radix

--- build/index.d.ts
+++ build/index.d.ts
@@ -1,10 +1,11 @@
 /**
  * Parse input exclusively as Safe Integer
  * @param s {String} The input
+ * @param radix {Number} The radix. (Optional) Default = 10;
  * @throws {RangeError} Input is more than Number.MAX_SAFE_INTEGER
  * @throws {RangeError} Input is less than Number.MIN_SAFE_INTEGER
  * @throws {TypeError} Input is not an Integer
  * @returns {Number} A Safe Integer
  */
-declare function parseInteger(s: string): number;
+declare function parseInteger(s: string, radix?: number): number;
 export { parseInteger as parseInteger };
