7025a7a6652f4b5d353ba584b07a15539cec7d45
MintedKitten <87274176+MintedKitten@users.noreply.github.com>
2023-02-12 16:43:49 +0700

    Style change in JSDocs

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 /**
- * Parse input exclusively as Safe Integer
- * @param s {string} The input
- * @throws {RangeError} Input is more than Number.MAX_SAFE_INTEGER
- * @throws {RangeError} Input is less than Number.MIN_SAFE_INTEGER
- * @throws {TypeError} Input is not an Integer
- * @returns {number} A Safe Integer
+ * Parse input exclusively as Safe Integer.
+ * @param s {string} The input.
+ * @throws {RangeError} Input is more than Number.MAX_SAFE_INTEGER.
+ * @throws {RangeError} Input is less than Number.MIN_SAFE_INTEGER.
+ * @throws {TypeError} Input is not an Integer.
+ * @returns {number} A Safe Integer.
  */
 function parseInteger(s: string): number {
   const nm = Number(s);
